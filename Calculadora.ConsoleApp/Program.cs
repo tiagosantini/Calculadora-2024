5ed8d336712876e711d2e7a9fd9d8cf26fca4327
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-03-19 16:00:00 -0300

    Refatorando aplicação para utilizar funções

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -32,70 +32,92 @@
         {
             while (true)
             {
-                Console.Clear();
+                string operacao = MostrarMenu();
 
-                Console.WriteLine("Calculadora Tabajara 2024\n");
-
-                Console.WriteLine("Digite 1 para Adicionar");
-                Console.WriteLine("Digite 2 para Subtrair");
-                Console.WriteLine("Digite 3 para Multiplicar");
-                Console.WriteLine("Digite 4 para Dividir");
-
-                Console.WriteLine("Digite S para sair");
-
-                string operacao = Console.ReadLine();
-
-                // || = comparação não obrigatória (só um precisa ser verdadeiro) 
-                if (operacao == "S" || operacao == "s")
+                if (OpcaoSaidaSelecionada(operacao))
                     break;
 
-                // && = comparação obrigatória
-                if (operacao != "1" && operacao != "2" && operacao != "3" && operacao != "4" && operacao != "S" && operacao != "s")
+                else if (OpcaoInvalida(operacao))
                 {
-                    Console.WriteLine("Operação inválida, tente novamente...");
-                    Console.ReadLine();
-
+                    ExibirMensagemErro();
                     continue;
                 }
 
-                Console.WriteLine("Digite o primeiro número:");
+                else
+                    RealizarCalculo(operacao);
+            }
+        }
 
-                string primeiroNumeroString = "";
+        // procedimento = conjunto de instruções que executam em sequência
+        // função = procedimento que retorna um valor
+        static string MostrarMenu()
+        {
+            Console.Clear();
 
-                primeiroNumeroString = Console.ReadLine();
+            Console.WriteLine("Calculadora Tabajara 2024\n");
 
-                double primeiroNumero = Convert.ToDouble(primeiroNumeroString);
+            Console.WriteLine("Digite 1 para Adicionar");
+            Console.WriteLine("Digite 2 para Subtrair");
+            Console.WriteLine("Digite 3 para Multiplicar");
+            Console.WriteLine("Digite 4 para Dividir");
 
-                Console.WriteLine("Digite o segundo número:");
+            Console.WriteLine("Digite S para sair");
 
-                string segundoNumeroString = Console.ReadLine();
+            string operacao = Console.ReadLine();
 
-                double segundoNumero = Convert.ToDouble(segundoNumeroString);
+            return operacao;
+        }
 
-                double resultado = 0;
+        // parâmetros / argumentos
+        static bool OpcaoSaidaSelecionada(string opcao)
+        {
+            bool opcaoSaidaSelecionada = opcao == "S" || opcao == "s";
 
-                if (operacao == "1")
-                {
+            return opcaoSaidaSelecionada;
+        }
+
+        static bool OpcaoInvalida(string opcao)
+        {
+            bool opcaoInvalida = opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4" && opcao != "S" && opcao != "s";
+
+            return opcaoInvalida;
+        }
+
+        static void ExibirMensagemErro()
+        {
+            Console.WriteLine("Operação inválida, tente novamente...");
+            Console.ReadLine();
+        }
+
+        static void RealizarCalculo(string operacao)
+        {
+            Console.WriteLine("Digite o primeiro número:");
+
+            double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+
+            double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = 0;
+
+            switch (operacao)
+            {
+                case "1":
                     resultado = primeiroNumero + segundoNumero;
-                }
+                    break;
 
-                else if (operacao == "2")
-                {
+                case "2":
                     resultado = primeiroNumero - segundoNumero;
-                }
+                    break;
 
-                else if (operacao == "3")
-                {
+                case "3":
                     resultado = primeiroNumero * segundoNumero;
-                }
+                    break;
 
-                else if (operacao == "4")
-                {
-                    // checar se o segundo número é 0
-
+                case "4":
                     while (segundoNumero == 0)
                     {
-                        // caso for 0, pedir para o usuário digitar de novo
                         Console.WriteLine("Segundo número não pode ser ZERO, tente novamente!");
 
                         Console.ReadLine();
@@ -105,14 +127,18 @@
                         segundoNumero = Convert.ToInt32(Console.ReadLine());
                     }
 
-                    // caso não, divir
                     resultado = primeiroNumero / segundoNumero;
-                }
-
-                Console.WriteLine("O resultado é: " + resultado);
-
-                Console.ReadLine();
+                    break;
             }
+
+            ExibirResultado(resultado);
+        }
+
+        static void ExibirResultado(double resultado)
+        {
+            Console.WriteLine("O resultado é: " + resultado);
+
+            Console.ReadLine();
         }
     }
 }
